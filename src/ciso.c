8a6273f1a26ce6fd71894fb16343fae3ed4a520e
sindastra <1236984+sindastra@users.noreply.github.com>
2019-12-15 17:26:38 +0100

    Update headers

--- src/ciso.c
+++ src/ciso.c
@@ -1,24 +1,21 @@
-/*
-    This file is part of Ciso.
+/**
+ * mCISO - Continuation of the original CISO
+ * Copyright (C) 2005  BOOSTER
+ * Copyright (C) 2019  Sindastra <https://github.com/sindastra/psp-mciso/>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
 
-    Ciso is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    Ciso is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Foobar; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-
-    Copyright 2005 BOOSTER
-*/
-
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
